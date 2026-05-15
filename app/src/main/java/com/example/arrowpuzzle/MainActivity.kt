5aef9becd65d49e7da1ff7f6ccfed76a30dc25a1
UsamaSabirrr <usamasabir4321@gmail.com>
2026-05-16 04:05:38 +0500

    feat: implement initial arrow grid logic created the arrow logic, but this one is wrong. it's creating individual arrows, but we actually need connected path for each arrow and draw it on canvas?

--- app/src/main/java/com/example/arrowpuzzle/MainActivity.kt
+++ app/src/main/java/com/example/arrowpuzzle/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.example.arrowpuzzle
 
+import android.R
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.arrowpuzzle.ui.theme.ArrowPuzzleTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +35,133 @@ class MainActivity : ComponentActivity() {
         setContent {
             ArrowPuzzleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    GameScreen()
                 }
             }
         }
     }
 }
 
+enum class Direction{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
+data class Cell(val row: Int, val col: Int,val direction: Direction)
+
+val dummyBoard = listOf(
+    listOf(
+        Cell(0, 0, Direction.RIGHT),
+        Cell(0, 1, Direction.DOWN),
+        Cell(0, 2, Direction.LEFT),
+        Cell(0, 3, Direction.UP),
+    ),
+//    listOf(
+//        Cell(1, 0, Direction.UP),
+//        Cell(1, 1, Direction.RIGHT),
+//        Cell(1, 2, Direction.DOWN),
+//        Cell(1, 3, Direction.LEFT),
+//    ),
+//    listOf(
+//        Cell(2, 0, Direction.LEFT),
+//        Cell(2, 1, Direction.UP),
+//        Cell(2, 2, Direction.RIGHT),
+//        Cell(2, 3, Direction.DOWN),
+//    )
+)
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GameScreen(){
+    Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        dummyBoard.forEach { row->Row{
+            row.forEach { cell->
+                ArrowCell(cell)
+            }
+        }}
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ArrowPuzzleTheme {
-        Greeting("Android")
+fun ArrowCell(cell: Cell){
+    Box(modifier = Modifier.size(80.dp).padding(4.dp),contentAlignment = Alignment.Center){
+        Box(modifier = Modifier.background(Color.Yellow).fillMaxSize(),)
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            drawArrow(cell.direction)
+        }
     }
+}
+
+fun DrawScope.drawArrow(direction: Direction) {
+
+    val padding = 10f
+
+    val centerX = size.width / 2
+    val centerY = size.height / 2
+
+    val start: Offset
+    val end: Offset
+
+    when (direction) {
+
+        Direction.RIGHT -> {
+
+            start = Offset(
+                padding,
+                centerY
+            )
+
+            end = Offset(
+                size.width - padding,
+                centerY
+            )
+        }
+
+        Direction.LEFT -> {
+
+            start = Offset(
+                size.width - padding,
+                centerY
+            )
+
+            end = Offset(
+                padding,
+                centerY
+            )
+        }
+
+        Direction.UP -> {
+
+            start = Offset(
+                centerX,
+                size.height - padding
+            )
+
+            end = Offset(
+                centerX,
+                padding
+            )
+        }
+
+        Direction.DOWN -> {
+
+            start = Offset(
+                centerX,
+                padding
+            )
+
+            end = Offset(
+                centerX,
+                size.height - padding
+            )
+        }
+    }
+
+    drawLine(
+        color = Color.Blue,
+        start = start,
+        end = end,
+        strokeWidth = 8f,
+        cap = StrokeCap.Round
+    )
 }
